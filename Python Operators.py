7ca9c9dea5d533e774ffab5f38e5f56983d0d114
Ittesaf-Rahman <ittesafrahman2011@gmail.com>
2025-04-09 11:20:19 +0600

    Add files via upload

---
+++ Python Operators.py
@@ -0,0 +1,31 @@
+#Arithmetic Operators(+, -, *, /, **, //, %)
+a = 2 + 3
+print(a)
+a = 2
+b = 3
+c = a + b
+print(c)
+a = 3 - 2
+print(a)
+#Assignmment Operators
+x = 2
+x += 2
+print(x)
+x = 2
+x **= 4
+print(x)
+#Comparison Operator(==, >, <, >=, <=)
+x = 4
+y = 3
+z = 4
+i = y < z
+j = x > z
+print(i)
+print(j)
+
+#Logical Operator(and, or, not)
+x = 4
+y = 3
+z = 2
+i = x > y and z < y
+print(i)
